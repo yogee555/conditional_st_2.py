bc0216f5eece82f06fb12d5cf1189c4f36cf3d5f
YOGEESH K P <148324344+yogee555@users.noreply.github.com>
2023-12-09 23:19:23 +0530

    Add files via upload

---
+++ conditional_st_2.py
@@ -0,0 +1,12 @@
+# conditional statement_2
+
+a = 1
+b = 2
+if a>b:
+    print("if a is greater than b print if")
+elif a<b:
+    print("if a is less than b print elif")
+elif a==b:
+    print("if a is equal than b print elif")
+else:
+    print("if not upper print else")
